87731a5a600bc67c5f3c3e485a2b88037bdccb36
Dravia Vemal <contact@draviavemal.com>
2023-11-29 18:39:20 +0000

    - Theme model update

---
+++ Presentation/Models/PowerPointModel.cs
@@ -0,0 +1,21 @@
+namespace OpenXMLOffice.Presentation;
+public class PowerPointProperties
+{
+    public PowerPointTheme? Theme;
+}
+
+public class PowerPointTheme
+{
+    public string? Dark1;
+    public string? Light1;
+    public string? Dark2;
+    public string? Light2;
+    public string? Accent1;
+    public string? Accent2;
+    public string? Accent3;
+    public string? Accent4;
+    public string? Accent5;
+    public string? Accent6;
+    public string? Hyperlink;
+    public string? FollowedHyperlink;
+}
