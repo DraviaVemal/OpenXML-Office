c8c8627a7fcc553176816774d94a553e2f404b09
Dravia Vemal <contact@draviavemal.com>
2023-12-12 13:56:41 +0000

    Chart Setup Update

--- Global/Charts/BarChart .cs
+++ Global/Charts/BarChart .cs
@@ -1,5 +1,20 @@
+using DocumentFormat.OpenXml.Drawing.Charts;
+using DocumentFormat.OpenXml.Office2013.Drawing.ChartStyle;
+
 namespace OpenXMLOffice.Global;
 public class BarChart : BarFamilyChart
 {
+    public ChartSpace GetChartSpace()
+    {
+        return CreateChartSpace();
+    }
 
+    public ChartStyle GetChartStyle()
+    {
+        return CreateChartStyles();
+    }
+    public ColorStyle GetColorStyle()
+    {
+        return CreateColorStyles();
+    }
 }
--- Presentation/Internal/components/Chart.cs
+++ Presentation/Internal/components/Chart.cs
@@ -1,10 +1,10 @@
 using OpenXMLOffice.Global;
+using OpenXMLOffice.Excel;
 using DocumentFormat.OpenXml;
 using DocumentFormat.OpenXml.Packaging;
-using DocumentFormat.OpenXml.Drawing.Charts;
+using C = DocumentFormat.OpenXml.Drawing.Charts;
 using P = DocumentFormat.OpenXml.Presentation;
 using A = DocumentFormat.OpenXml.Drawing;
-using OpenXMLOffice.Excel;
 
 namespace OpenXMLOffice.Presentation;
 public class Chart
@@ -19,14 +19,27 @@ public class Chart
     {
         CurrentSlide = Slide;
         OpenXMLChartPart = Slide.GetSlidePart().AddNewPart<ChartPart>(Slide.GetNextSlideRelationId());
-        OpenXMLChartPart.AddNewPart<ChartStylePart>(GetNextChartRelationId());
-        OpenXMLChartPart.AddNewPart<ChartColorStylePart>(GetNextChartRelationId());
-        OpenXMLChartPart.AddNewPart<EmbeddedPackagePart>(EmbeddedPackagePartType.Xlsx.ContentType, GetNextChartRelationId());
+        InitialiseChartParts();
     }
 
+    private void InitialiseChartParts()
+    {
+        GetChartPart().AddNewPart<ChartStylePart>(GetNextChartRelationId());
+        GetChartPart().AddNewPart<ChartColorStylePart>(GetNextChartRelationId());
+        GetChartPart().AddNewPart<EmbeddedPackagePart>(EmbeddedPackagePartType.Xlsx.ContentType, GetNextChartRelationId());
+    }
     private ChartPart GetChartPart()
     {
         return OpenXMLChartPart;
+    }
+
+    private ChartStylePart GetChartStylePart()
+    {
+        return OpenXMLChartPart.ChartStyleParts.FirstOrDefault()!;
+    }
+    private ChartColorStylePart GetChartColorStylePart()
+    {
+        return OpenXMLChartPart.ChartColorStyleParts.FirstOrDefault()!;
     }
     internal string GetNextChartRelationId()
     {
@@ -38,17 +51,29 @@ public class Chart
         CurrentSlide = Slide;
     }
 
-    public P.GraphicFrame CreateChart(GlobalConstants.ChartTypes ChartTypes)
+    public P.GraphicFrame CreateChart(GlobalConstants.ChartTypes ChartTypes, DataCell[][] DataRows, ChartSetting? chartSetting = null)
     {
-        ChartSpace ChartSpace = new();
         switch (ChartTypes)
         {
-            default:
-                ChartBase ChartBase = new();
-                ChartSpace = ChartBase.CreateChartSpace();
+            case GlobalConstants.ChartTypes.BAR:
+                BarChart BarChart = new();
+                GetChartPart().ChartSpace = BarChart.GetChartSpace();
+                GetChartStylePart().ChartStyle = BarChart.GetChartStyle();
+                GetChartColorStylePart().ColorStyle = BarChart.GetColorStyle();
                 break;
         }
-        GetChartPart().ChartSpace = ChartSpace;
+        // Load Data To Embeded Sheet
+        Stream stream = GetChartPart().EmbeddedPackagePart!.GetStream();
+        Spreadsheet spreadsheet = new(stream, SpreadsheetDocumentType.Workbook);
+        Worksheet Worksheet = spreadsheet.AddSheet();
+        int RowIndex = 1;
+        foreach (DataCell[] DataCells in DataRows)
+        {
+            Worksheet.SetRow(RowIndex, 1, DataCells);
+            ++RowIndex;
+        }
+        spreadsheet.Save();
+        // Load Chart Part To Graphics Frame For Export
         string? relationshipId = CurrentSlide.GetSlidePart().GetIdOfPart(GetChartPart());
         P.NonVisualGraphicFrameProperties NonVisualProperties = new()
         {
@@ -72,41 +97,14 @@ public class Chart
                 }),
             Graphic = new A.Graphic(
                 new A.GraphicData(
-                    new ChartReference { Id = relationshipId }
+                    new C.ChartReference { Id = relationshipId }
                 )
                 { Uri = "http://schemas.openxmlformats.org/drawingml/2006/chart" })
         };
+        // Save All Changes
         GetChartPart().ChartSpace.Save();
-        Stream stream = GetChartPart().EmbeddedPackagePart!.GetStream();
-        Spreadsheet spreadsheet = new(stream, SpreadsheetDocumentType.Workbook);
-        Worksheet Worksheet = spreadsheet.AddSheet();
-        Worksheet.SetRow(1, 1, new DataCell[5]{
-            new(){
-                CellValue = "test1",
-                DataType = CellDataType.STRING
-            },
-             new(){
-                CellValue = "test2",
-                DataType = CellDataType.STRING
-            },
-             new(){
-                CellValue = "test3",
-                DataType = CellDataType.STRING
-            },
-             new(){
-                CellValue = "test4",
-                DataType = CellDataType.STRING
-            },
-             new(){
-                CellValue = "test5",
-                DataType = CellDataType.STRING
-            }
-        }, new RowProperties()
-        {
-            height = 20
-        });
-        spreadsheet.Save();
-        GetChartPart().ChartSpace.Save();
+        GetChartStylePart().ChartStyle.Save();
+        GetChartColorStylePart().ColorStyle.Save();
         return GraphicFrame;
     }
 
