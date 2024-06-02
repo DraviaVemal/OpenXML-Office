ba3538715029c26f83c2e46ffe3f39abd9030d70
DraviaVemal <contact@draviavemal.com>
2024-06-02 15:21:19 +0000

    Starting point

---
+++ Spreadsheet/Components/2007/Shape.cs
@@ -0,0 +1,12 @@
+// Copyright (c) DraviaVemal. Licensed under the MIT License. See License in the project root.
+
+namespace OpenXMLOffice.Spreadsheet_2007
+{
+    /// <summary>
+    /// Shape Class For Presentation shape manipulation
+    /// </summary>
+    public class Shape
+    {
+
+    }
+}
