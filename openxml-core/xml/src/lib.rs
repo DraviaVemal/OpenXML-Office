c2f89bc0c360d80fe78da110167199f9dacdf5ad
Dravia Vemal <contact@draviavemal.com>
2024-10-16 12:12:28 +0000

    Baseline setting up all supported language projects

---
+++ openxml-core/xml/src/lib.rs
@@ -0,0 +1,4 @@
+pub fn add(left: u64, right: u64) -> u64 {
+    left + right
+}
+
---
+++ openxml-cs/Spreadsheet/Excel.cs
@@ -0,0 +1,10 @@
+﻿namespace OpenXMLOffice.Spreadsheet_2007
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Excel
+    {
+
+    }
+}
