0a0bbfb8521a645775cf7d611f80d2c3efca145e
Dravia Vemal <contact@draviavemal.com>
2024-10-20 02:29:52 +0000

    Experimenting the file system

---
+++ openxmloffice-core/xml/src/structs/common.rs
@@ -0,0 +1,17 @@
+use crate::enums::common::NodeContentType;
+
+pub(crate) struct CurrentNode {
+    name: String,
+    content_type: NodeContentType,
+    childs: Option<Vec<CurrentNode>>,
+}
+
+/**
+ * This contains the root document to work with
+ */
+pub(crate) struct OpenXmlFile {
+    pub(crate) file_path: Option<String>,
+    pub(crate) temp_file_path: String,
+    pub(crate) is_readonly: bool,
+    pub(crate) archive_files: Vec<CurrentNode>,
+}
---
+++ openxmloffice-core/xml/src/utils/mod.rs
@@ -0,0 +1,2 @@
+pub mod file_handling;
+pub mod strings;
